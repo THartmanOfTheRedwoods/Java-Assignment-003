3822c2ea8018978cc598438da7a74ce77503d5a4
Rigel <rigelcluciani@gmail.com>
2024-02-04 23:11:52 -0800

    2/4/24 RL

--- src/GuessStarter.java
+++ src/GuessStarter.java
@@ -1,15 +1,19 @@
 import java.util.Random;
-
+import java.util.Scanner;
 /**
  * Starter code for the "Guess My Number" exercise.
  */
 public class GuessStarter {
-
     public static void main(String[] args) {
+        // adding a Scanner
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter a number");
+        int input = Integer.parseInt(s.nextLine());
         // pick a random number
         Random random = new Random();
         int number = random.nextInt(100) + 1;
-        System.out.println(number);
+        System.out.println("I was thinking of the number "+number);
+        System.out.println(input-number);
     }
 
 }
