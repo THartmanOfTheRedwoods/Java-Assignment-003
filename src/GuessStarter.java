d4ab106065b118f466c8a3c15e8e0fa87186427e
Juanc <Juancervantesmrz69@gmail.com>
2024-02-04 22:01:37 -0800

    Completed Assignment 003

--- src/GuessStarter.java
+++ src/GuessStarter.java
@@ -8,8 +8,22 @@ public class GuessStarter {
     public static void main(String[] args) {
         // pick a random number
         Random random = new Random();
+        System.out.println("Im thinking of a number between 1 and 100. Can you guess what it is?");
+        System.out.print("The number I was thinking of is: ");
+        int guessnumber = random.nextInt (100) + 1;
+        System.out.println(guessnumber);
+        System.out.print("The number is: ");
         int number = random.nextInt(100) + 1;
         System.out.println(number);
+        int difference;
+        difference = (guessnumber - number);
+        System.out.print("the difference is: ");
+        System.out.println(guessnumber - number);
+
+
+
+
+
     }
 
 }
